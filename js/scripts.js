71d8d2774157c8030f8a5ac0920d03897c7ee203
Mark Lawson <mlawson3691@gmail.com>
2016-08-26 10:37:02 -0700

    create an Order object that tracks the total price for all pizzas and displays it to the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 // BUSINESS LOGIC:
+  function Order(pizzas, price) {
+    this.pizzas = pizzas;
+    this.price = price;
+  }
+  var allPizzas = [];
+  var currentOrder = new Order(allPizzas, 0);
 
   function Pizza(size, toppings) {
     this.size = size;
@@ -7,7 +13,6 @@
 
   Pizza.prototype.calculate = function() {
     var cost;
-
     if (this.size === "Sm") {
       cost = 8;
     } else if (this.size === "Md") {
@@ -15,17 +20,15 @@
     } else {
       cost = 12;
     }
-
     for (var i = 0; i < this.toppings.length; i++) {
       cost += 1;
     }
-
+    currentOrder.price += cost;
     return cost;
   }
 
 // USER INTERFACE LOGIC:
 $(function() {
-
   $("#addPizza").click(function(event) {
     event.preventDefault();
     var currentSize = $("input:radio[name=size]:checked").val();
@@ -34,15 +37,16 @@ $(function() {
       currentToppings.push($(this).val());
     });
     var newPizza = new Pizza(currentSize, currentToppings);
+    var cost = newPizza.calculate();
     $("#pizzas").append(
       "<li>" +
-        "<h4>" + newPizza.size + " Pizza - $" + newPizza.calculate() + ".00</h4>" +
+        "<h4>" + newPizza.size + " Pizza - $" + cost + ".00</h4>" +
         "<ul class='toppingsDisplay'></ul>" +
       "</li>");
     for (var i = 0; i < newPizza.toppings.length; i++) {
       $(".toppingsDisplay:last").append("<li>" + newPizza.toppings[i] + "</li>");
     }
-
+    $("#totalPrice").text(currentOrder.price);
   });
 
   $("#delivery").click(function() {
